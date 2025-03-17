629aaf78c9932ce2f9575ce679cbf3bb84f03f65
gangadharrsi <59114329+gangadharrsi@users.noreply.github.com>
2025-03-17 08:34:42 +0530

    Initial commit from your app

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+// Top-level build.gradle.kts (Project level)
+    plugins {
+      id("com.android.application") version "8.3.0" apply false // Specify version and apply false
+      id("org.jetbrains.kotlin.android") version "1.9.22" apply false // Specify version and apply false
+    }
+
+    // Add repositories here
+    repositories {
+      google()
+      mavenCentral()
+    }
